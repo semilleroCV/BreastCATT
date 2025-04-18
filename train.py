fe19f457e44318060dabd0660379ab465d507cb1
Majinka10 <camachopinto_@hotmail.com>
2025-04-18 11:37:41 -0500

    Remove local main process check for state saving in training logic

--- train.py
+++ train.py
@@ -642,8 +642,7 @@ def main():
             output_dir = f"epoch_{epoch}"
             if args.output_dir is not None:
                 output_dir = os.path.join(args.output_dir, output_dir)
-            if accelerator.is_local_main_process:
-                accelerator.save_state(output_dir)
+            accelerator.save_state(output_dir)
 
     if args.with_tracking:
         accelerator.end_training()
