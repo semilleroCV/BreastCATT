8688829ddd937c1c4db6ba67ddb1bf4c10873f76
Majinka10 <camachopinto_@hotmail.com>
2025-05-11 13:50:55 -0500

    Set default output directory to 'runs' in parse_args for model storage

--- train.py
+++ train.py
@@ -138,7 +138,7 @@ def parse_args():
     parser.add_argument(
         "--num_warmup_steps", type=int, default=0, help="Number of steps for the warmup in the lr scheduler."
     )
-    parser.add_argument("--output_dir", type=str, default=None, help="Where to store the final model.")
+    parser.add_argument("--output_dir", type=str, default="runs", help="Where to store the final model.")
     parser.add_argument("--seed", type=int, default=None, help="A seed for reproducible training.")
     parser.add_argument("--push_to_hub", type=str2bool, nargs="?", const=False, default=False, help="Whether or not to push the model to the Hub.")
     parser.add_argument(
